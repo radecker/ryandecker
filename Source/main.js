38b5735cfce244c4e8a2ec566f6e8f2a4306d892
Ryan <Ryan>
2017-12-07 01:28:45 -0600

    initial commit

---
+++ Source/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	setTimeout(function(){
+ 		$('#69').fadeOut(1500);
+  		setTimeout(function(){
+    		$('#69').text("Ryan Decker");
+    		$('#69').fadeIn(1500);
+  		}, 1500)
+	}, 1500);
+});
